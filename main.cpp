690715255437a5a10d1c2398d1287ecde59fd51b
cosmin <gcboeriu@gmail.com>
2023-04-05 12:42:07 +0300

    beta 2

--- main.cpp
+++ main.cpp
@@ -38,6 +38,10 @@ public:
         myPoint.position = sf::Vector2f(x, y);
         return input;
     }
+    friend ostream& operator<<(ostream& output, Point& myPoint) {
+        cout << myPoint.position.x << ' ' << myPoint.position.y;
+        return output;
+    }
     void set_color( sf::Color col ){
         this->color = col;
     }
@@ -63,9 +67,8 @@ public:
 class Line{
     Point *endpoints;
 public:
-    explicit Line(Point *v=nullptr): endpoints(v){}
-    Line(const Point& a, const Point& b){
-        endpoints = new Point;
+    explicit Line(const Point& a = Point(), const Point& b = Point()){
+        endpoints = new Point[2];
         *endpoints = a;
         *(endpoints+1) = b;
     }
@@ -81,6 +84,14 @@ public:
             this->endpoints[1] = other.endpoints[1];
         }
         return *this;
+    }
+    friend istream& operator>>(istream& input, Line& myLine) {
+        cin>>myLine.endpoints[0]>>myLine.endpoints[1];
+        return input;
+    }
+    friend ostream& operator<<(ostream& output, Line& myLine) {
+        cout << myLine.endpoints[0] <<' '<< myLine.endpoints[1];
+        return output;
     }
     void drawLine(sf::RenderWindow& wind){
         sf::Vertex line[] = {endpoints[0].get_position(), endpoints[1].get_position()};
@@ -115,19 +126,19 @@ public:
         }
         return false;
     }
-    sf::Vector2f get_collision(Line& l){
+    Point get_collision(Line& l){
         // n1 - n2 = m2 x - m1 x = ( m2 - m1 ) x
         if( abs(this->get_tangent() - l.get_tangent() ) < 0.001f ){
-            return { -1, -1};
+            return Point(-1, -1);
         }
         else {
             float x = (this->get_reminder() - l.get_reminder()) / (l.get_tangent() - this->get_tangent());
             float y = this->get_tangent() * x + this->get_reminder();
             Point a(x, y);
             if (l.test_point_inclusion(a) && this->test_point_inclusion(a)) {
-                return {x, y};
+                return Point(x, y);
             } else {
-                return {-1, -1};
+                return Point(-1, -1);
             }
         }
     }
@@ -146,35 +157,40 @@ public:
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Light Simulator!");
+    window.setFramerateLimit(60);
     // const float EPSILON = 0.01;
-    Line *obstacles;
-    int numberOfObstacles = 2;
-    obstacles[0] = Line( Point(301.0f, 301.1f, sf::Color(255, 0, 0)), Point(800.1f, 1000.6f, sf::Color(255, 0, 0)) );
-    obstacles[1] = Line( Point(301.0f, 301.1f, sf::Color(255, 0, 0)), Point(100.1f, 1400.6f, sf::Color(255, 0, 0)) );
+    int numberOfObstacles = 4;
+    Line *obstacles = new Line[numberOfObstacles];
+    for( int i = 0; i < numberOfObstacles; i++ ){
+        cin>>obstacles[i];
+    }
+    /*
+    1 1 800 800
+    1 800 800 1
+    1890 300 439 567
+    600 900 300 1200
+    */
+    //cout<<obstacles[1];
     SourcePoint sourcep;
     while( window.isOpen() ) {
         sf::Event event{};
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {window.close();}
-
         }
         sourcep.update_position(window);
-        for( int i = 1; i < 360; i += 2 ){
+        for( int i = 1; i <= 360; i += 2 ){
+            Point p(sourcep, float(float(i) * PI / 180), 10000);
+            Line *l = new Line(sourcep, p);
             for( int j = 0; j < numberOfObstacles; j++ ) {
-                Point p(sourcep, float(float(i) * PI / 180), 500);
-                Line *l = new Line(sourcep, p);
-                (*l).set_color(sf::Color(255, 255, 0));
-                sf::Vector2f v = obstacles[j].get_collision(*l);
-                if (v.x != -1) {
+                Point v = obstacles[j].get_collision(*l);
+                //cout<<j<<' '<<v<<endl;
+                if (v.get_position().x != -1) {
                     delete l;
-                    l = new Line(sourcep, Point(v.x, v.y));
-                    (*l).set_color(sf::Color(255, 255, 0));
-                    (*l).drawLine(window);
-                }
-                else {
-                    (*l).drawLine(window);
+                    l = new Line(sourcep, Point(v.get_position().x, v.get_position().y));
                 }
             }
+            (*l).set_color(sf::Color(255, 255, 0));
+            (*l).drawLine(window);
         }
         for( int i = 0; i < numberOfObstacles; i++ ) {
             obstacles[i].drawLine(window);
